2a4adf585a9c8525974d6108160ea2ab3b90a40e
Chrb09 <carloshrbarile@gmail.com>
2024-10-09 22:48:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/inclusipet/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.inclusipet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.inclusipet.ui.theme.InclusipetTheme
+import com.example.inclusipet.ui.theme.inter
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            App()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            fontSize = 50.sp,
+            fontFamily = inter,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun App() {
+    InclusipetTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/inclusipet/ui/theme/Color.kt
@@ -0,0 +1,6 @@
+package com.example.inclusipet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple100 = Color(0xFF574DBD)
+val Grey100 = Color(0xFF404040)
